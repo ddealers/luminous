8a6a975ff9e4059847738b79895da44e82a32de8
Wenceslao Negrete <vampediem@gmail.com>
2014-08-27 12:38:58 -0500

    skrollr

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	//===== Mobile =====
+	function detectmob() { 
+		if( navigator.userAgent.match(/Android/i)
+		|| navigator.userAgent.match(/webOS/i)
+		|| navigator.userAgent.match(/iPhone/i)
+		|| navigator.userAgent.match(/iPad/i)
+		|| navigator.userAgent.match(/iPod/i)
+		|| navigator.userAgent.match(/BlackBerry/i)
+		|| navigator.userAgent.match(/Windows Phone/i)
+		){
+		 	return true;
+		}else {
+			return false;
+		}
+	}
+	/*
+	if(detectmob()){
+		$("video").replaceWith("<div class='bgvid'></div>");
+	}
+	*/
+
+	//===== Init Skrollr =====
+	var s = skrollr.init({
+		smoothScrolling: true,
+		mobileDeceleration: 0.004
+	});
+	skrollr.menu.init(s);
+});
